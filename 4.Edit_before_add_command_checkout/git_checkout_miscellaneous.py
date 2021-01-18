911e7b57efafbcdbf45a4c9a0e245d7968a69c1f
rythmstories <gauravgupta1051996@gmail.com>
2021-01-19 02:28:32 +0530

    git miscellaneous points

---
+++ 4.Edit_before_add_command_checkout/git_checkout_miscellaneous.py
@@ -0,0 +1,10 @@
+# Here we need to understand that - 
+
+#         All the changes ate local, nothing is reflected in repository universally, we can check this on webportal. 
+
+#         After git checkout command we will be back at the last or latest commit changes in the file 
+
+#                 ie in git checkout single-file, we will reach to last commit state of the file and all the changes will be lost 
+
+#                 also in git checkout multiple files, we will reach to last commit state for all files and folders, 
+#                     as it is done universallly on whole git local repository. 
---
+++ 5.Edit_after_commit_revert/git_revert_miscellaneous.py
@@ -0,0 +1,6 @@
+# Here we need to understand that - 
+
+#         All the changes are local if we use '-n' parameter.
+
+#         If '-n' parameter is not used at revert command, all the changes done will be committed automatically. 
+
