da6695429e2f38c42d1fb12b67688651efc72073
rythmstories <gauravgupta1051996@gmail.com>
2021-01-18 19:21:57 +0530

    push git complete

---
+++ 3.first_change/git_2_push.py
@@ -0,0 +1,12 @@
+# Till yet we have committed our changes but they are not reflected in the repository at web portal and universally. 
+
+# Last step after committing changes is to PUSH code. Command for pushing code is - 
+
+# git push
+
+
+# Here you need to enter your credentials like username and password, so be ready with username and password. 
+
+print("git push")
+print("\n")
+print("Changes will be reflected in the repository universally.")
