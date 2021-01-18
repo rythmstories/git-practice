b4f4250a6121f6f25e27b8eb2878b9970ee5fc56
rythmstories <gauravgupta1051996@gmail.com>
2021-01-18 16:43:07 +0530

    check

---
+++ 1.Cloning/git_clone1.py
@@ -0,0 +1,5 @@
+# First in GIT is to create an account in any GIT based software like Github / Gitlab etc. 
+
+# My personal favorite is GITHUB because of its user-friendliness and because of its industry acceptance. 
+
+print("Lets start!!!")
