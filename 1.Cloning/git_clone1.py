7c2f06ad505e1d8733d7e3622a9930e05ed12f7d
rythmstories <gauravgupta1051996@gmail.com>
2021-01-18 18:21:20 +0530

    clone complete

--- 1.Cloning/git_clone1.py
+++ 1.Cloning/git_clone1.py
@@ -3,3 +3,47 @@
 # My personal favorite is GITHUB because of its user-friendliness and because of its industry acceptance. 
 
 print("Lets start!!!")
+print("\n")
+
+# Go to repository web page. 
+
+# Check for cloning options. 
+
+# There will 2 options to clone - 
+
+#       https
+#       SSH 
+
+# HTTPS : 
+# Copy Https clone link by clicking on copy.
+# Here we can clone easily by the below command at local CMD - 
+
+# git clone (copy link)
+
+# eg - git clone https://github.com/rythmstories/git-practice.git
+
+print("Https Git clone ")
+print("\n")
+print("git clone https://github.com/rythmstories/git-practice.git")
+
+
+print("\n")
+
+# SSH :
+# Copy HHS clone link by clicking on copy. 
+# Here we have to go through below command on local cmd. 
+
+# git clone (copy link)
+
+# eg - git clone https://github.com/rythmstories/git-practice.git
+
+print("SSH Git clone ")
+print("\n")
+print("git clone https://github.com/rythmstories/git-practice.git")
+
+# Here you will need to enter your credentials of Github. 
+
+
+
+# It is preferable to go with 
+
